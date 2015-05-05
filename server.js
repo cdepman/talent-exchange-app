2c609710a6207c0a17aa71a185fa895ff8ecb807
Charlie Depman <cdepman@gmail.com>
2015-05-05 13:08:59 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan        = require('morgan');
+var cookieParser  = require('cookie-parser');
+var bodyParser    = require('body-parser');
+var session       = require('express-session');
+
+var configDB = require('./config/database.js')
+
+
+// config
+mongoose.connect(configDB.url);
+
+// require('./config/passport')(passport);
+
+
+// express
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.set('view engine', 'ejs');
+
+// passport
+app.use(session({ secret: 'thebestkeptsecreteveryay'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// routes
+require('./app/routes.js')(app, passport);
+
+// 3...2...1...launch!
+app.listen(port);
+console.log('Listening on port', port);
